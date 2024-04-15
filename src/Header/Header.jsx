390bb44146147b050a0c38982cdc868093277ebc
dvlpr2003 <gayathrigaya698@gmail.com>
2024-04-15 21:09:48 +0530

    header

---
+++ src/Header/Header.jsx
@@ -0,0 +1,24 @@
+
+import "./Header.css"
+
+export default function Header(){
+    return(
+        <header>
+            <div id="Header-main">
+                <div>
+                    
+                </div>
+                <div id="insta-options">
+                    <span>Video Downloader</span>
+                    <span>Photo Downloader</span>
+                    <span>Reels Downloader</span>
+                    <span>Story Saver</span>
+                    <span>Story Viewer</span>
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </header>
+    )
+}
