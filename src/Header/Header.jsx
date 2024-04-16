7f5b24e474326976f0dfd199d031e744d950a290
dvlpr2003 <gayathrigaya698@gmail.com>
2024-04-16 20:17:07 +0530

    header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,12 +1,11 @@
 
 import "./Header.css"
-
 export default function Header(){
     return(
         <header>
             <div id="Header-main">
-                <div>
-                    
+                <div id="header-logo-container">
+                    <img src='logo.svg' alt="" />
                 </div>
                 <div id="insta-options">
                     <span>Video Downloader</span>
