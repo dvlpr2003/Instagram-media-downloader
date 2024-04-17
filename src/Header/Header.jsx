1225935db4436f546c21364464292871d87e649f
dvlpr2003 <gayathrigaya698@gmail.com>
2024-04-17 21:15:45 +0530

    header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -14,7 +14,15 @@ export default function Header(){
                     <span>Story Saver</span>
                     <span>Story Viewer</span>
                 </div>
-                <div>
+                <div id="container-3">
+                    <span>FAQ</span>
+                    <button id="language-btn">
+                        <span>ENG</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="currentColor" id="arrow-svg">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+</svg>
+
+                    </button>
 
                 </div>
             </div>
