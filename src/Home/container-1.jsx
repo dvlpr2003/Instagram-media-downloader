80eead791e22c8d16cd9fc215b7a3e1751ca5dad
dvlpr2003 <gayathrigaya698@gmail.com>
2024-04-18 21:50:32 +0530

    section-1

--- src/Home/container-1.jsx
+++ src/Home/container-1.jsx
@@ -4,7 +4,39 @@ import "./container.css"
 export default function Section1(){
     return(
         <section id="instagram-downloader-home-1">
+            <Section1Elements/>
 
         </section>
+    )
+}
+
+function Section1Elements(){
+    return(
+        <div id="section-elements">
+            <div id="section-elements-nav-option" >
+                <a href="#" style={{border:"none"}}>
+                    <img src="photo.svg" alt="photo" />
+                    <span>Photo</span>
+                </a>
+                <a href="#">
+
+                    <img src="reels.svg" alt="reels"  />
+                    <span>Reels</span>
+                </a>
+                <a href="#">
+                    <img src="video.svg" alt="video" />
+                    <span>Video</span>
+                </a>
+                <a href="#">
+                    <img src="story.svg" alt="story" />
+                    <span>Story</span>
+                </a>
+                <a href="#">
+                    <img src="highlights.svg" alt="Highlights" />
+                    <span>Highlights</span>
+                </a>
+            </div>
+
+        </div>
     )
 }
