6d77a3c81c96842f6107d0370a8fff22f44e49e4
dvlpr2003 <gayathrigaya698@gmail.com>
2024-04-21 08:39:12 +0530

    section-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import Header from "./Header/Header"
 import Section1 from "./Home/container-1"
+import Section2Main from "./Home/container-2"
 function App() {
 
   return (
     <>
     {/* <Header/> */}
-    <Section1/>
+    {/* <Section1/> */}
+    <Section2Main/>
 
     </>
   )
---
+++ src/Home/container-2.jsx
@@ -0,0 +1,21 @@
+
+import "./container2.css"
+
+
+export default function Section2Main(){
+    return(
+        <section id="section2-main">
+            <Section2Heading/>
+        </section>
+    )
+}
+
+function Section2Heading(){
+    return(
+        <div className="section-2-heading">
+            <h2 className="section-2-heading w4-c7">Instagram Downloader <span className="w4-c7-span-head">Features</span></h2>
+            <p className="w4-c7-p">Below, discover the key functionalities of our Instagram Downloader service. Among them, the following stand out</p>
+            
+        </div>
+    )
+}
