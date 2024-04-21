3aa33dadbe740a27fe3c68c097ce636ecf1b94a4
dvlpr2003 <gayathrigaya698@gmail.com>
2024-04-21 13:38:19 +0530

    section-1

--- src/Home/container-2.jsx
+++ src/Home/container-2.jsx
@@ -6,6 +6,7 @@ export default function Section2Main(){
     return(
         <section id="section2-main">
             <Section2Heading/>
+            <Section2Body/>
         </section>
     )
 }
@@ -16,6 +17,28 @@ function Section2Heading(){
             <h2 className="section-2-heading w4-c7">Instagram Downloader <span className="w4-c7-span-head">Features</span></h2>
             <p className="w4-c7-p">Below, discover the key functionalities of our Instagram Downloader service. Among them, the following stand out</p>
             
+        </div>
+    )
+}
+
+function Section2Body(){
+    return(
+        <div className="section-2-body-main">
+            <div className="w4-c7-s-b-main">
+                <div className="w4-c7-s-b-txt">
+                    <a href="#" className="w4-c7-s-b-txt-a">
+                    Instagram Photo Downloader
+                    </a>
+                    <p className="w4-c7-s-b-txt-p">
+                    Effortlessly access and download Instagram photos anonymously, preserving their original resolution or opting for reduced sizes, all free of charge. With our service, downloaded images are conveniently stored for easy access whenever you need them.
+                    </p>
+
+
+                </div>
+                <div className="w4-c7-s-b-img-container" >
+                    <img src="img/insta-img.png" alt="instagram-photo-downloader" className="w4-c7-s-b-img"/>
+                </div>
+            </div>
         </div>
     )
 }
