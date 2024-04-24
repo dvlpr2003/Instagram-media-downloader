532d61e3c38d8a887144f9e6c2ad1f9242fcd320
dvlpr2003 <gayathrigaya698@gmail.com>
2024-04-24 21:13:46 +0530

    body

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,8 +1,10 @@
 import Section1 from "./container-1"
 import Section2Main from "./container-2"
+import Header from "../Header/Header"
 export default function Home(){
     return(
         <>
+        <Header/>
         <Section1/>
         <Section2Main/>
 
