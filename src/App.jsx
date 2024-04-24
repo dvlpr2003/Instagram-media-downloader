610ceef8f0c40878c5f3391e6a8b58081584ffec
dvlpr2003 <gayathrigaya698@gmail.com>
2024-04-24 21:09:54 +0530

    body

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@
 import Header from "./Header/Header"
 import Section1 from "./Home/container-1"
 import Section2Main from "./Home/container-2"
+import Home from "./Home/Home"
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
 function App() {
 
   return (
     <>
     {/* <Header/> */}
     {/* <Section1/> */}
-    <Section2Main/>
+    {/* <Section2Main/>
+     */}
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
+    </BrowserRouter>
 
     </>
   )
---
+++ src/Home/Home.jsx
@@ -0,0 +1,11 @@
+import Section1 from "./container-1"
+import Section2Main from "./container-2"
+export default function Home(){
+    return(
+        <>
+        <Section1/>
+        <Section2Main/>
+
+        </>
+    )
+}
