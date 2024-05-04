6edc6e9e40ddbc6aa5e8d79d0a6ff278df7bc9a2
dvlpr2003 <gayathrigaya698@gmail.com>
2024-05-04 13:52:05 +0530

    -r

--- src/Home/container-1.jsx
+++ src/Home/container-1.jsx
@@ -68,7 +68,9 @@ function Section1Input(){
         <div id="input-w3-c7-main">
             <div id="input-w3-c7-main-element">
                 <input type="text" className="input-w3-c7" placeholder="https.//"/>
-                <button type="submit" className="input-w3-c7-btn"><div class="loader"></div></button>
+                <button type="submit" className="input-w3-c7-btn">
+                <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+                    </button>
             </div>
 
         </div>
