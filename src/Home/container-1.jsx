a3bea3680a35846f97f9731fddc5c03b06589ee2
dvlpr2003 <gayathrigaya698@gmail.com>
2024-05-20 11:40:18 +0530

    -r

--- src/Home/container-1.jsx
+++ src/Home/container-1.jsx
@@ -3,6 +3,9 @@ import axios from "axios"
 import "./container.css"
 import { useState } from "react";
 
+const delay = ms => new Promise(
+    resolve => setTimeout(resolve, ms)
+  );
 export default function Section1({Heading,setHeading}){
     const [Link,setLink]=useState("")
     const [isLoading,setLoading]=useState(false)
@@ -131,19 +134,27 @@ function Result({ExtractedData}){
         </div>
     )
 }
-{/* <div class="loader"></div> */}
 
 function ResultOutput({img_src,video_src}){
+    // const [loadDWBtn,setloadDWBtn]=useState(false)
+    // function ActiveLoad(){
+    //     setloadDWBtn(true)
+    //     delay(2000)
+    //     setloadDWBtn(false)
+    // }
     return(
         <div className="result-sub img">
 <img src={img_src} alt="Instagram Image" />
         <div className="svg-container-w3-c7">
+            {/* <div class="loader"></div> */}
+            <div className="svg-container-w3-c7-child">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
 <path strokeLinecap="round" strokeLinejoin="round" d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z" />
 </svg>
 
 
-<a href={video_src} download={true} target="new">Download.mp4</a>
+<a href={video_src} download={true} target="new" className="w3-c7-download-btn">Download.mp4</a>
+</div>
 
         </div>
     </div>
