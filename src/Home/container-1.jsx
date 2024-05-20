98dd68db1a748ac8aed5b4a10c1714756c81546e
dvlpr2003 <gayathrigaya698@gmail.com>
2024-05-20 20:30:25 +0530

    -r

--- src/Home/container-1.jsx
+++ src/Home/container-1.jsx
@@ -10,6 +10,7 @@ export default function Section1({Heading,setHeading}){
     const [Link,setLink]=useState("")
     const [isLoading,setLoading]=useState(false)
     const [ExtractedData,setExtractedData]=useState(null)
+    // console.log(ExtractedData)
     async function getDada(){
         const regex = /\/reel\/([^/]+)\//;
         const regex2 = /\/p\/([^/]+)\//;
@@ -23,7 +24,7 @@ export default function Section1({Heading,setHeading}){
         try{
             setLoading(true)
             const response = await axios.get(`http://127.0.0.1:8000/api/download-instagram-post/${uniqueIdentifier}`);
-            console.log(response.data[0])
+            // console.log(response.data)
             setExtractedData(response.data)
 
             setLoading(false)
@@ -129,34 +130,48 @@ function Section1Input({ClickEvent,setLink,isLoading}){
 function Result({ExtractedData}){
     return(
         <div className="result">
-            <ResultOutput img_src = {ExtractedData[0].url.video_img_1} video_src = {ExtractedData[0].url.video_1}/>
+            {/* <ResultOutput img_src = {ExtractedData[0].url.video_img_1} video_src = {ExtractedData[0].url.video_1}/> */}
+            {ExtractedData.map((e,i)=><ResultOutput data ={e} index ={i} key = {i}/>)}
             {/* <ResultOutput/> */}
         </div>
     )
 }
 
-function ResultOutput({img_src,video_src}){
+function ResultOutput({data,index}){
     // const [loadDWBtn,setloadDWBtn]=useState(false)
     // function ActiveLoad(){
     //     setloadDWBtn(true)
     //     delay(2000)
     //     setloadDWBtn(false)
+    
     // }
+
     return(
         <div className="result-sub img">
-<img src={img_src} alt="Instagram Image" />
+            {
+
+                data.url.video ?<img src={`${data.url.video_img}`} alt="Instagram Image" /> :<img src={data.url.img_c5} alt="Instagram Image" />
+            }
         <div className="svg-container-w3-c7">
             {/* <div class="loader"></div> */}
             <div className="svg-container-w3-c7-child">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+        {data.url.video ?<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
 <path strokeLinecap="round" strokeLinejoin="round" d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z" />
+</svg>:
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
 </svg>
 
+}
 
-<a href={video_src} download={true} target="new" className="w3-c7-download-btn">Download.mp4</a>
+
+
+{data.url.video ?<a href={data.url.video_c5} download={true} target="new" className="w3-c7-download-btn">Download video</a>:<a href={data.url.img_c5} className="w3-c7-download-btn" download={true}>Download image</a>}
 </div>
 
         </div>
     </div>
+
+
     )
 }
