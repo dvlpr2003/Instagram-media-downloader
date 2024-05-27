37428c773016e2a1ee530586ed553c9b3096c6e8
dvlpr2003 <gayathrigaya698@gmail.com>
2024-05-27 18:04:19 +0530

    -t

--- src/Home/container-1.jsx
+++ src/Home/container-1.jsx
@@ -11,6 +11,7 @@ export default function Section1({Heading,setHeading}){
     const [Link,setLink]=useState("")
     const [isLoading,setLoading]=useState(false)
     const [ExtractedData,setExtractedData]=useState(null)
+    const [ProfileInfo,setProfileInfo] = useState(null)
     // console.log(ExtractedData)
     async function getDada(){
         const regex = /\/reel\/([^/]+)\//;
@@ -38,7 +39,8 @@ export default function Section1({Heading,setHeading}){
                 setLoading(true)
 
                 const response = await axios.get(`http://127.0.0.1:8000/api/get-profile-info/${extractname}/`)
-                console.log(response)
+                console.log(response.data)
+                setProfileInfo(response.data)
                 setLoading(false)
 
             }
@@ -61,9 +63,12 @@ export default function Section1({Heading,setHeading}){
         <div id="app">
         <section id="instagram-downloader-home-1">
             {
-         (ExtractedData === null)?<Section1Elements Heading={Heading} setHeading={setHeading} ClickEvent={ClickEvent} setLink={setLink} isLoading={isLoading}/>:""}
-            {ExtractedData && <Result ExtractedData={ExtractedData}/>}
-            {/* <ProfileResult/> */}
+         (ExtractedData === null )&& (ProfileInfo === null)?<Section1Elements Heading={Heading} setHeading={setHeading} ClickEvent={ClickEvent} setLink={setLink} isLoading={isLoading}/>:''}
+            {ExtractedData &&<Result ExtractedData={ExtractedData}/>}
+            {ProfileInfo&& <ProfileResult ProfileInfo={ProfileInfo}/>}
+         {/* {(ProfileInfo === null)?<Section1Elements Heading={Heading} setHeading={setHeading} ClickEvent={ClickEvent} setLink={setLink} isLoading={isLoading}/>:''} */}
+            
+
         </section>
 
         {ExtractedData &&<DownloadAgain setExtractedData={setExtractedData}/>}
@@ -135,18 +140,6 @@ function Section1Input({ClickEvent,setLink,isLoading}){
         </div>
     )
 }
-
-
-
-
-
-
-
-
-
-
-
-
 function Result({ExtractedData}){
     return(
         <>
@@ -209,22 +202,22 @@ Download more
 
 
 
-function ProfileResult(){
+function ProfileResult({ProfileInfo}){
     return(
         <div className="profile-result-w3-c4">
             <div className="profile-result-w3-c4-image">
-                <img src="img/dev.jpeg" alt="" />
+                <img src={ProfileInfo.profile_pic_url} alt="" />
                 <div className="profile-result-w3-c4-image-dwn">
                     <span>Download Profile Image</span>
                 </div>
             </div>
             <div className="profile-result-w3-c4-follow" >
-                <div>followers</div>
-                <div>following</div>
-                <div>post</div>
+                <div>{ProfileInfo.followers}followers</div>
+                <div>{ProfileInfo.following}following</div>
+                <div>{ProfileInfo.posts}post</div>
             </div>
             <div className="profile-result-w3-c4-txt-clip">
-                <div className="profile-result-w3-c4-txt"> hello
+                <div className="profile-result-w3-c4-txt">{ProfileInfo.bio}
                 <div className="profile-result-w3-c4-svg-container">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 c7-clip">
   <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5A3.375 3.375 0 0 0 6.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0 0 15 2.25h-1.5a2.251 2.251 0 0 0-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 0 0-9-9Z" />
